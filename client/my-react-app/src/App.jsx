74ea3c8e0d3af941caeb6491df4d357574c158ad
Damian antony <jemcydami55@gmail.com>
2025-02-24 15:47:14 +0530

    remove unused login form from App component

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -8,13 +8,7 @@ function App() {
 
   return (
     <>
-     <div className="login">
-     <form >
-      <input type="text" name="name" placeholder="Name" />
-      <input type="email" name="email" placeholder="Email" />
-      <button type="submit">Submit</button>
-    </form>
-     </div>
+    
     </>
   )
 }
